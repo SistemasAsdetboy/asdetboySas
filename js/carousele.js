ec6a4ade6c2b8d8b41ac3f8bbb62c5c82084843c
Julian Contreras <julian.contreras@repremundo.com.co>
2024-07-11 12:28:22 -0500

    Preparo pagina nueva

--- js/carousele.js
+++
@@ -1,6 +0,0 @@
-var angle = 0;
-function galleryspin(sign) { 
-spinner = document.querySelector("#spinner");
-if (!sign) { angle = angle + 61.16; } else { angle = angle - 61.16; }
-spinner.setAttribute("style","-webkit-transform: rotateY("+ angle +"deg); -moz-transform: rotateY("+ angle +"deg); transform: rotateY("+ angle +"deg);");
-}
